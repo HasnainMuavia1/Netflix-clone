8f7fcb1267556b92ae6377015bb0f331474609fd
Hasnain Muavia <hasnain.muavia2004@gmail.com>
2024-07-07 13:45:04 +0500

    first commit

---
+++ Netflix/javaScript.js
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+          $(".netflix-navbar").css("background" , "#0C0C0C");
+        }
+  
+        else{
+            $(".netflix-navbar").css("background" , "transparent");  	
+        }
+    });
+
+  })
+
+
+function position(id){
+  var card = document.getElementsByClassName('card')[id];
+  // card.style.transform = 'scale(1.5)';
+  console.log(id)
+}
+=======
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+          $(".netflix-navbar").css("background" , "#0C0C0C");
+        }
+  
+        else{
+            $(".netflix-navbar").css("background" , "transparent");  	
+        }
+    });
+
+  })
+
+
+function position(id){
+  var card = document.getElementsByClassName('card')[id];
+  // card.style.transform = 'scale(1.5)';
+  console.log(id)
+}
+>>>>>>> 8c2b9a847903942a113f7b6563e0f133336891b5
